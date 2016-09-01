0035fae21cb1b950c667bbffe271056ae63f7c07
Ben Howard <bhoward@scottlogic.co.uk>
2016-09-01 10:14:50 +0100

    added webpack for bundling project files
    Change-Id: Ia3dbae470ac5f0ea5a8c353a1a59920e9776b52c

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,48 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jscs");
     grunt.loadNpmTasks("grunt-mocha-test");
     grunt.loadNpmTasks("grunt-mocha-istanbul");
+    grunt.loadNpmTasks("grunt-webpack");
 
     var files = ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js"];
     var artifactsLocation = "build_artifacts";
+
+    var webpack = require("webpack");
 
     grunt.initConfig({
         jshint: {
             all: files,
             options: {
-                jshintrc: true
+                jshintrc: true,
+                ignores: ["./public/js/bundle.js"]
+            }
+        },
+        webpack: {
+            ChatApp: {
+                entry: "./public/js/ChatApp",
+                output: {
+                    path: "./public/js/",
+                    filename: "bundle.js",
+                },
+                module: {
+                    loaders: [{
+                        test: /\.css$/,
+                        loader: "style-loader!css-loader"
+                    }]
+                },
+                plugins: [
+                    new webpack.optimize.UglifyJsPlugin({
+                        minimize: true
+                    })
+                ]
             }
         },
         jscs: {
-            all: files
+            all: files,
+            options: {
+                excludeFiles: ["./public/js/bundle.js"]
+            }
         },
         mochaTest: {
             test: {
@@ -61,16 +88,17 @@ module.exports = function(grunt) {
         grunt.util.spawn({
             cmd: cmd,
             args: [istanbulPath, "report", "--dir=" + options.coverageFolder]
-        }, function(err) {
+        }, function (err) {
             if (err) {
                 return done(err);
             }
             done();
         });
     });
-
+    grunt.registerTask("bundle", ["webpack:ChatApp"]);
     grunt.registerTask("check", ["jshint", "jscs"]);
-    grunt.registerTask("test", ["check", "mochaTest", "mocha_istanbul", "istanbul_report",
-        "istanbul_check_coverage"]);
+    grunt.registerTask("test", ["check", "bundle", "mochaTest", "mocha_istanbul", "istanbul_report",
+        "istanbul_check_coverage"
+    ]);
     grunt.registerTask("default", "test");
 };
---
+++ public/js/ChatApp.js
@@ -0,0 +1,6 @@
+/*global require*/
+
+angular.module("ChatApp", ["ngAnimate", "ngMaterial", "toastr"]);
+
+require("./ChatController.js");
+require("../css/main.css");
