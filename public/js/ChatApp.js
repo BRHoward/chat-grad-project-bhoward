a53093003a69a5c9c6bde158731cf38acf2dcb7c
Ben Howard <bhoward@scottlogic.co.uk>
2016-09-02 08:59:46 +0100

    pulled conversation logic into seperate service
    Change-Id: I452f33223ff3b8c4ac84c4d200246ef1ee486edc

--- public/js/ChatApp.js
+++ public/js/ChatApp.js
@@ -4,4 +4,5 @@ angular.module("ChatApp", ["ngAnimate", "ngMaterial", "toastr"]);
 
 require("./ChatController.js");
 require("./RequestService.js");
+require("./ConversationService.js");
 require("../css/main.css");
