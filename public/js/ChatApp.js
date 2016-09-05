3bff968961e9f88284d43314da1e02dcc3bba0f7
Ben Howard <bhoward@scottlogic.co.uk>
2016-09-05 11:25:29 +0100

    added config file for palettes
    Change-Id: Iba7a3ce97a86ea0cfbbffbf466446c56cb4c0d59

--- public/js/ChatApp.js
+++ public/js/ChatApp.js
@@ -4,5 +4,6 @@ angular.module("ChatApp", ["ngAnimate", "ngMaterial", "toastr"]);
 
 require("./ChatController.js");
 require("./RequestService.js");
+require("./ChatConfig.js");
 require("./ConversationService.js");
 require("../css/main.css");
